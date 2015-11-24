7c8b4a060bc2a69b7684480896c9d0c17ec57e4b
Noneking <aanoriega.mobile@gmail.com>
2015-11-24 12:57:52 +0100

    date picker and image chooser made

--- app/src/main/java/com/example/mantenimiento/anoriega_ie_8_2_1/InsertActivity.java
+++ app/src/main/java/com/example/mantenimiento/anoriega_ie_8_2_1/InsertActivity.java
@@ -1,18 +1,48 @@
 package com.example.mantenimiento.anoriega_ie_8_2_1;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class InsertActivity extends Activity {
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class InsertActivity extends Activity implements ImageButton.OnClickListener, Date_Picker.OnFechaSeleccionada {
+
+    private static final int SELECT_PICTURE = 1;
+
+    public String selectedImagePath;
+
+    ImageButton imageButton;
+    Button buttonDate;
+    TextView textViewDate;
+
+    Calendar c = Calendar.getInstance();
+    int year = c.get(Calendar.YEAR);
+    int month = c.get(Calendar.MONTH);
+    int day = c.get(Calendar.DAY_OF_MONTH);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        initComponents();
+        initListeners();
     }
 
     @Override
@@ -37,6 +67,19 @@ public class InsertActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void initComponents()
+    {
+        imageButton= (ImageButton) findViewById(R.id.imageButtonInsert);
+        buttonDate= (Button) findViewById(R.id.insert_buttonDate);
+        textViewDate= (TextView) findViewById(R.id.insert_textViewDate);
+    }
+
+    public void initListeners()
+    {
+        imageButton.setOnClickListener(this);
+        buttonDate.setOnClickListener(this);
+    }
+
     public void insertDate(View v)
     {
 
@@ -50,4 +93,59 @@ public class InsertActivity extends Activity {
         finish();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.imageButtonInsert:
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+                break;
+            case R.id.insert_buttonDate:
+                    Date_Picker date=new Date_Picker();
+                    date.show(getFragmentManager(),"DIALOGO_FECHA");
+
+
+                    //DialogFragment mDialog = new DialogFragment();
+                    //mDialog.show(new DatePicker(this));
+                    //DialogFragment dialogFragment = new DialogFragment();
+                    //dialogFragment.show(getFragmentManager(), "start_date_picker");
+
+                break;
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                //selectedImagePath = getPath(selectedImageUri);
+                imageButton.setImageURI(selectedImageUri);
+            }
+        }
+    }
+
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null )
+        {
+            return null;
+        }
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if(cursor!= null)
+        {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return uri.getPath();
+    }
+
+    @Override
+    public void onResultadoFecha(GregorianCalendar fecha) {
+        textViewDate.setText(fecha.get(Calendar.DAY_OF_MONTH)+"/"+(fecha.get(Calendar.MONTH)+1)+"/"+fecha.get(Calendar.YEAR));
+    }
 }
